2661aa283ba7ad6043e6cf93125b8058952e51b6
Nicholas Thiago Xavier Costa <nicholasxavis23@gmail.com>
2024-02-25 22:14:14 -0700

    Implements: - RegistrationFactory methods

--- src/ca/sait/crs/services/RealRegistrationService.java
+++ src/ca/sait/crs/services/RealRegistrationService.java
@@ -6,43 +6,46 @@ import ca.sait.crs.contracts.Student;
 
 import java.util.ArrayList;
 
-
 /**
  * Registers student with course.
+ * 
  * @author Nick Hamnett <nick.hamnett@sait.ca>
  * @since June 1, 2023
  */
 public class RealRegistrationService implements RegistrationService {
-    private final ArrayList<Registration> registrations;
+	private final ArrayList<Registration> registrations;
 
-    public RealRegistrationService() {
-        this.registrations = new ArrayList<>();
-    }
+	public RealRegistrationService() {
+		this.registrations = new ArrayList<>();
+	}
 
-    /**
-     * Registers student with course
-     * @param student Student Student instance
-     * @param course Course Course instance
-     * @return Registration instance.
-     */
-    @Override
-    public Registration register(Student student, Course course) throws CannotCreateRegistrationException {
-        // TODO: Create instance of RegistrationFactory.
-        // TODO: Call build() method in RegistrationFactory instance to handle validating parameters and creating new Registration object.
-        // Do not catch CannotCreateRegistrationException in this method.
-        Registration registration = new ca.sait.crs.models.Registration(course, student);
+	/**
+	 * Registers student with course
+	 * 
+	 * @param student Student Student instance
+	 * @param course  Course Course instance
+	 * @return Registration instance.
+	 */
+	@Override
+	public Registration register(Student student, Course course) throws CannotCreateRegistrationException {
+		// TODO: Create instance of RegistrationFactory.
+		// TODO: Call build() method in RegistrationFactory instance to handle
+		// validating parameters and creating new Registration object.
+		// Do not catch CannotCreateRegistrationException in this method.
+		Registration registration = new ca.sait.crs.factories.RegistrationFactory().build(course, student);
 
-        this.registrations.add(registration);
+		this.registrations.add(registration);
 
-        return registration;
-    }
+		return registration;
+	}
 
-    /**
-     * Gets registrations.
-     * @return All registrations
-     */
-    @Override
-    public ArrayList<Registration> getRegistrations() {
-        return this.registrations;
-    }
+	/**
+	 * Gets registrations.
+	 * 
+	 * @return All registrations
+	 */
+	@Override
+	public ArrayList<Registration> getRegistrations() {
+		return this.registrations;
+	}
 }
